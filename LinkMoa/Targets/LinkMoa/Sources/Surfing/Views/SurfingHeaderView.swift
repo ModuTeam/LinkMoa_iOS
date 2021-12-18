fe2b4cbfd2f763b2d5d659024244c745cd7b7e53
Beomcheol Kwon <bc6793@gmail.com>
2021-12-18 21:58:24 +0900

    refactor: SurfingHeaderView Xib to Codebase

--- LinkMoa/Targets/LinkMoa/Sources/Surfing/Views/SurfingHeaderView.swift
+++ LinkMoa/Targets/LinkMoa/Sources/Surfing/Views/SurfingHeaderView.swift
@@ -6,15 +6,48 @@
 //
 
 import UIKit
+import LinkMoaKit
+import SnapKit
 
-class SurfingHeaderView: UICollectionReusableView {
+final class SurfingHeaderView: UICollectionReusableView, Reusable {
 
-    static let reuseableViewIndetifier: String = "SurfingHeaderView"
+    let moreButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "chevron.backward"), for: .normal)
+        button.setTitle("더보기", for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.tintColor = UIColor(rgb: 0x364788)
+        return button
+    }()
     
-    @IBOutlet weak var moreButton: UIButton!
-    @IBOutlet weak var titleLabel: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .notoSansMedium(size: 18)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        addSubview(moreButton)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(self).inset(18)
+            make.height.equalTo(27)
+            make.centerY.equalTo(self)
+        }
+        
+        moreButton.snp.makeConstraints { make in
+            make.centerY.equalTo(self)
+            make.right.equalTo(self).inset(18)
+        }
+    }
 }
